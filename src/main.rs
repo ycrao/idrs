231a2fa3233dea824f3e7593f512af3bcf04fe30
raoyc <raoyc2009@gmail.com>
2018-05-12 22:58:36 +0800

    add get_age,get_gender fn

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,50 @@ extern crate chrono;
 // use std::path::Path;
 // use sqlite::State;
 use regex::Regex;
-// use std::cmp::Ordering;
 use std::string::String;
 use chrono::prelude::*;
+use std::error::Error;
+use std::fmt;
+use std::io;
 
 fn main() {
-    let id = "  42032319930606629x  ";
-    validate(id);
+    println!("please input id the identity card (eg 42032319930606629x ): ");
+    let mut id = String::new();  // mutable 可改变的
+
+    io::stdin().read_line(&mut id)
+            .expect("Fail to read line");
+
+    id = id.trim().parse().unwrap();
+    if id.is_empty() == true {
+        println!("empty identity card, give you a default case: 42032319930606629x");
+        id = "42032319930606629x".to_string();
+    }
+    let identity = &id;
+    println!("----------");
+    println!("identity card number: {}", identity);
+    let is_ok = validate(identity).is_ok();
+    println!("validation passed: {:?}", is_ok);
+
+    /*
+    match validate(identity) {
+        Ok(ok) => println!("validation passed: {:?}", ok),
+        Err(e) => println!("error: {:?}", e),
+    };
+    */
+
+    if is_ok == true {
+        let constellation = get_constellation(identity);
+        println!("constellation: {}", constellation);
+        let gender = get_gender(identity);
+        println!("gender: {}", gender);
+        let birthday = get_birth(identity);
+        println!("birthday: {}", birthday);
+        let age = get_age(identity);
+        println!("age: {}", age);
+    } else {
+        let e = validate(identity).err().unwrap();
+        println!("error: {:?}", e);
+    }
 }
 
 
@@ -23,34 +60,32 @@ struct Division {
 }
 */
 
-
 fn db() {
     let connetion = sqlite::open("../db/id.sqlite").unwrap();
+
 }
 
 
-fn validate(id: &str) {
-    let _id = id.trim().to_uppercase();
-    println!("identity-card is {}", _id);
-    if regex_match_id(&_id) == true {
-        println!("regex passed!");
-        if pass_checksum(&_id) == true {
-            println!("checksum passed!");
-        } else {
-            println!("id checksum error!");
-        }
-        let date : String = _id.chars().skip(6).take(8).collect();
-        let new_date = date.to_string() + " 00:00:00";
-        // println!("date {:?}, new_date {:?}", date, new_date);
-        let check_date = Utc.datetime_from_str(&new_date, "%Y%m%d %H:%M:%S").is_ok();
-        // println!("{:?}", check_date);
-        if check_date == true {
-            println!("date passed!");
-        } else {
-            println!("date illegal!");
-        }
+pub fn trim(id: &str) -> String {
+    let identity = id.trim().to_uppercase();
+    identity
+}
+
+pub fn validate(id: &str) -> Result<bool, &'static str> {
+    let identity = trim(&id);
+    if regex_match_id(&identity) == false {
+        return Err("identity card illegal!");
+    }
+    if pass_checksum(&identity) == false {
+        return Err("identity card checksum error!");
+    }
+    let date : String = identity.chars().skip(6).take(8).collect();
+    let new_date = date.to_string() + " 00:00:00";
+    let check_date = Utc.datetime_from_str(&new_date, "%Y%m%d %H:%M:%S").is_ok();
+    if check_date == true {
+        return Ok(true);
     } else {
-        println!("id illegal!");
+        return Err("identity card with wrong date!");
     }
 }
 
@@ -62,33 +97,99 @@ fn regex_match_id(id: &str) -> bool {
 
 
 fn pass_checksum(id: &str) -> bool {
-    let mut _id : [char; 18] = ['0'; 18];
+    let mut identity : [char; 18] = ['0'; 18];
     for (index, c) in (id.chars()).enumerate() {
-        _id[index] = c;
+        identity[index] = c;
     }
-    let _factor : [u8; 17] = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
-    let _verify : [char; 11] = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
-    let mut _checksum : u32 = 0;
-    let mut _i : u8 = 0;
-    let _last = _id[17];
-    for _i in 0..17 {
-        let _elm = _id[_i];
-        let _t1 = (_elm.to_digit(10).unwrap()) as u32;
-        let _t2 = _factor[_i] as u32;
-        let _t3 = (_t1*_t2) as u32;
-        // println!("_elm {:?}, _i {}, _checksum {} , _t1 {} , _t2 {} , _t3 {}", _elm, _i, _checksum, _t1, _t2, _t3);
-        _checksum = _checksum + _t3;
+    let factor : [u8; 17] = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+    let verify : [char; 11] = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+    let mut checksum : u32 = 0;
+    let mut i : u8 = 0;
+    let last = identity[17];
+    for i in 0..17 {
+        let elm = identity[i];
+        let t1 = (elm.to_digit(10).unwrap()) as u32;
+        let t2 = factor[i] as u32;
+        let t3 = (t1*t2) as u32;
+        checksum = checksum + t3;
     }
-    let _mod = (_checksum%11) as usize;
-    let verify = _verify[_mod];
-    // println!("_checksum {}, _mod {}, verify {}, _last {}", _checksum, _mod, verify, _last);
-    verify == _last
+    let m = (checksum%11) as usize;
+    verify[m] == last
+}
+
+
+pub fn get_constellation(id: &str) -> String {
+    let identity = trim(&id);
+    let month : String = identity.chars().skip(10).take(2).collect();
+    let day : String = identity.chars().skip(12).take(2).collect();
+    let edge_days : [u32; 12] = [21, 20, 21, 20, 21, 22, 23, 23, 23, 24, 22, 21];
+    let constellations = [
+                            "水瓶座",  // 1.21-2.19 [Aquarius]
+                            "双鱼座",  // 2.20-3.20 [Pisces]
+                            "白羊座",  // 3.21-4.19 [Aries]
+                            "金牛座",  // 4.20-5.20 [Taurus]
+                            "双子座",  // 5.21-6.21 [Gemini]
+                            "巨蟹座",  // 6.22-7.22 [Cancer]
+                            "狮子座",  // 7.23-8.22 [Leo]
+                            "处女座",  // 8.23-9.22 [Virgo]
+                            "天秤座",  // 9.23-10.23 [Libra]
+                            "天蝎座",  // 10.24-11.21 [Scorpio]
+                            "射手座",  // 11.22-12.20 [Sagittarius]
+                            "魔羯座",  // 12.21-1.20 [Capricorn]
+                        ];
+    let mut m = month.parse::<i32>().unwrap();
+    m = m - 1;
+    let mut idx = m as usize;
+    let d = day.parse::<u32>().unwrap();
+    if d < edge_days[idx] {
+        m = m - 1;
+    }
+    if m >= 0 {
+        idx = m as usize;
+        constellations[idx].to_string()
+    } else {
+        constellations[11].to_string()
+    }
+}
+
+fn get_gender(id: &str) -> String {
+    let identity = trim(&id);
+    let gender : String = identity.chars().skip(16).take(1).collect();
+    let g = gender.parse::<u8>().unwrap();
+    if g%2 == 0 {
+        "f".to_string()
+    } else {
+        "m".to_string()
+    }
 }
 
 /*
-fn check_birthday(year : i32, month : i32, day : i32) -> bool {
-    let dt = Utc.ymd(year, month, day);
-    let birthday = dt.format("%Y-%m-%d").to_string();
-    true
+fn get_area(id: &str) -> Area {
+    let _id = trim(&id);
+    let month : String = _id.chars().skip(10).take(2).collect();
 }
 */
+
+fn get_age(id: &str) -> i32 {
+    let identity = trim(&id);
+    let year : String = identity.chars().skip(6).take(4).collect();
+    let month : String = identity.chars().skip(10).take(2).collect();
+    let day : String = identity.chars().skip(12).take(2).collect();
+    let y = year.parse::<i32>().unwrap();
+    let m = month.parse::<u32>().unwrap();
+    let d = day.parse::<u32>().unwrap();
+    let birth : DateTime<Utc> = Utc.ymd(y, m, d).and_hms(0, 0, 0);
+    let t2 = birth.timestamp();
+    let now: DateTime<Utc> = Utc::now();
+    let t1 = now.timestamp();
+    let t3 = t1 - t2;
+    let diff : i32 = (t3/3_153_6000) as i32;
+    return diff;
+}
+
+fn get_birth(id: &str) -> String {
+    let identity = trim(&id);
+    let birthday : String = identity.chars().skip(6).take(8).collect();
+    birthday
+}
+
